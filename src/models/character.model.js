5fdc3fb80a23d22e6cc8abd40e94997a09061a99
Garcia Diego <garciadiegogabriel.gn@gmail.com>
2025-06-26 13:52:01 -0300

    Modelo de tablas para la base de datos

--- src/models/character.model.js
+++ src/models/character.model.js
@@ -1,1 +1,33 @@
 //MODELO DE LAS TABLAS
+  import sequelize from "../config/database.js";
+  import { DataTypes } from "sequelize";
+
+  const PERSONAJES = sequelize.define("Character", {
+      id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+      },  
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false 
+      },
+    ki: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+      },
+    race: { 
+      type: DataTypes.INTEGER,
+      allowNull: false
+      },
+    gender: {
+      type: DataTypes.STRING, 
+      allowNull: false
+      },
+    description: {
+      type: DataTypes.STRING 
+      }
+
+  });
+
+  export default PERSONAJES;
