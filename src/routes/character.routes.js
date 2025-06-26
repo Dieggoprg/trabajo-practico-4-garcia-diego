c47457045a5b17416aa522318be39b89ef8eea8a
Garcia Diego <garciadiegogabriel.gn@gmail.com>
2025-06-26 14:14:14 -0300

    CREACIÓN DE RUTAS VINCULADAS CON LAS FUNCIONALIDADES

--- src/routes/character.routes.js
+++ src/routes/character.routes.js
@@ -1,1 +1,23 @@
 //RUTAS  -- API
+import express from "express";
+const router = express.Router();
+
+//importo los los controladores y sus validaciones de la carpeta ./src/controllers/characters.controller.js
+import {
+  crearPersonajes,
+  traerTodosLosPJ,
+  traePersonajesXid,
+  actualizarPersonaje,
+  eliminarPersonaje,
+} from "../controllers/character.controller.js";
+
+
+//Creo las rutas con sus métodos 
+router.post("/characters/", crearPersonajes);
+router.get("/characters/", traerTodosLosPJ);
+router.get("/characters/:id", traePersonajesXid);
+router.put("/characters/:id", actualizarPersonaje);
+router.delete("/characters/:id", eliminarPersonaje);
+
+//exporto las rutas
+export default router;
